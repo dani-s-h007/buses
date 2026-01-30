20b54613bbaf45492a58052dae70a3b3993f9d87
dani-s-h007 <shanusathar@gmail.com>
2026-01-30 17:58:59 +0530

    Updated App.jsx and secured config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/buses/",  // <--- THIS MUST MATCH YOUR REPO NAME
+  // Custom domain upayogikkumbol base '/' mathram aayirikkanam
+  base: "/", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['fsevents'],
